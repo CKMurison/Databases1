505d18b8ee93f1463471635666d64989d393431b
Mergim <mergimberisha0@gmail.com>
2023-04-28 17:37:36 +0100

    created add and delete tests

--- social_network_challenge/lib/users_repository.rb
+++ social_network_challenge/lib/users_repository.rb
@@ -1,36 +1,49 @@
-require_relative 'users'
+require_relative "users"
 
 class UsersRepository
   def all
-  # Executes the SQL query:
-  sql = 'SELECT id, email_address, user_name FROM users;'
-  result_set = DatabaseConnection.exec_params(sql, [])
+    # Executes the SQL query:
+    sql = "SELECT id, email_address, user_name FROM users;"
+    result_set = DatabaseConnection.exec_params(sql, [])
 
-  users = []
+    users = []
 
-  result_set.each do |record|
-    user = Users.new
-    user.email_address = record['email_address']
-    user.user_name = record['user_name']
+    result_set.each do |record|
+      user = Users.new
+      user.email_address = record["email_address"]
+      user.user_name = record["user_name"]
 
-    users << user
+      users << user
     end
     return users
   end
 
   def find(id)
-    sql = 'SELECT id, email_address, user_name FROM users WHERE id = $1;'
+    sql = "SELECT id, email_address, user_name FROM users WHERE id = $1;"
     sql_params = [id]
     result_set = DatabaseConnection.exec_params(sql, sql_params)
 
-   record = result_set[0]
+    record = result_set[0]
 
-   user = Users.new
-   user.email_address = record['email_address']
-   user.user_name = record['user_name']
+    user = Users.new
+    user.email_address = record["email_address"]
+    user.user_name = record["user_name"]
 
     return user
   end
 
-  
+  def create(users)
+    sql = "INSERT INTO users (email_address, user_name) VALUES ($1, $2);"
+    sql_params = [users.email_address, users.user_name]
+    DatabaseConnection.exec_params(sql, sql_params)
+    return nil
+  end
+
+  def delete(id)
+    sql = "DELETE FROM users WHERE id = $1;"
+    sql_params = [id]
+    DatabaseConnection.exec_params(sql, sql_params)
+
+    return nil
+  end
 end
--- social_network_challenge/spec/users_repository_spec.rb
+++ social_network_challenge/spec/users_repository_spec.rb
@@ -1,39 +1,63 @@
 require "users_repository"
 
 RSpec.describe UsersRepository do
-
   def reset_users_table
-   seed_sql = File.read("spec/user_seeds.sql")
-   connection = PG.connect({ host: "127.0.0.1", dbname: "SN_database_test" })
-   connection.exec(seed_sql)
+    seed_sql = File.read("spec/user_seeds.sql")
+    connection = PG.connect({ host: "127.0.0.1", dbname: "SN_database_test" })
+    connection.exec(seed_sql)
   end
 
   before(:each) do
-     reset_users_table
+    reset_users_table
   end
 
   it "return the list of all users" do
-  repo = UsersRepository.new
+    repo = UsersRepository.new
 
-  user = repo.all
-  expect(user.length).to eq (2)
-  expect(user.first.user_name).to eq ('EpicCam')
-  expect(user.first.email_address).to eq 'cameron@gmail'
+    user = repo.all
+    expect(user.length).to eq (2)
+    expect(user.first.user_name).to eq ("EpicCam")
+    expect(user.first.email_address).to eq "cameron@gmail"
   end
 
-  it 'finds user 1' do
+  it "finds user 1" do
     repo = UsersRepository.new
     user = repo.find(1)
-    expect(user.email_address).to eq 'cameron@gmail'
-    expect(user.user_name).to eq 'EpicCam'
+    expect(user.email_address).to eq "cameron@gmail"
+    expect(user.user_name).to eq "EpicCam"
   end
 
-  it 'finds user 2' do
+  it "finds user 2" do
     repo = UsersRepository.new
     user = repo.find(2)
-    expect(user.email_address).to eq 'mergim@yahoo'
-    expect(user.user_name).to eq 'Mergz'
+    expect(user.email_address).to eq "mergim@yahoo"
+    expect(user.user_name).to eq "Mergz"
   end
 
-  
+  it "adds a new user" do
+    repo = UsersRepository.new
+
+    new_user = Users.new
+    new_user.email_address = "Jim@bing"
+    new_user.user_name = "Big Jim"
+
+    repo.create(new_user)
+
+    users = repo.all
+    last_user = users.last
+
+    expect(last_user.email_address).to eq "Jim@bing"
+    expect(last_user.user_name).to eq "Big Jim"
+  end
+  it "deletes a user" do
+    repo = UsersRepository.new
+
+    id_to_delete = 1
+
+    repo.delete(id_to_delete)
+
+    all_users = repo.all
+    expect(all_users.length).to eq 1
+    expect(all_users.first.id).to eq("2")
+  end
 end
